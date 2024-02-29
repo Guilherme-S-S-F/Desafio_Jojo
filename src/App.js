9ccca3a8b76b7207d3528f60799aebaf93cf4f80
Guilherme Filheiro <guilhermefilheiro@gmail.com>
2024-02-29 14:33:05 -0300

    Adicionado Título e Background

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1 className='title'>Quiz Jojo's Stand</h1>
+      <div className='form-area'>
+      </div>
     </div>
   );
 }
