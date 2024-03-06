56eb7a9b3ffa34985d9b289ac18230eed7bbfdf2
Gian Braga <gianbraga1802@gmail.com>
2024-03-06 13:45:55 -0300

    criando o componente Result

--- src/pages/page-form/index.js
+++
@@ -1,6 +0,0 @@
-
-export default function PageForm() {
-    return(
-        <div></div>
-    );
-}
--- src/pages/page-result/index.js
+++
@@ -1,6 +0,0 @@
-
-export default function PageResult() {
-    return(
-        <div></div>
-    );
-}
