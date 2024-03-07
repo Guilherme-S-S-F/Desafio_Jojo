665b93a0c25abb91dd40a9a4f1790c54c91ec74e
Gian Braga <gianbraga1802@gmail.com>
2024-03-07 12:28:15 -0300

    instalando o gh-pages

--- src/components/StandResult/index.js
+++ src/components/StandResult/index.js
@@ -18,7 +18,7 @@ export function StandResult() {
 
     const sound = "../../sounds/" + stands[result].sound;
 
-    const [play, {stop}] = useSound(sound, {volume: 0.05});
+    const [play, {stop}] = useSound(sound, {volume: 0.1});
 
     const handleStop = () => {
       stop();
